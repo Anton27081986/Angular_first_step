f42cec5377ec8ca00f7ecc8b82f0a02264f4a0e1
Anton <anton27081986@gmail.com>
2022-01-29 08:07:47 +0300

    add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    // styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
+    @ViewChild('form') form: NgForm;
+
     answers = [{
         type: 'yes',
         text: 'Да'
@@ -16,8 +18,33 @@ export class AppComponent {
         text: 'Нет'
     }];
 
-    submitForm(form: NgForm) {
-        console.log('submited', form);
+    defaultAnswer = 'no';
+    defauitCountry = 'ua';
 
+    formData = {};
+    isSubmited = false;
+
+    addRandEmail() {
+        const randEmail = 'anton@gmail.com';
+        // this.form.setValue({
+        //     user: {
+        //         pass: '',
+        //         email: randEmail
+        //     },
+        //     country: '',
+        //     answer: ''
+        // })
+
+        this.form.form.patchValue({
+            user: { email: randEmail }
+        })
     }
+
+    submitForm() {
+        this.isSubmited = true;
+        this.formData = this.form.value;
+        this.form.reset();
+    }
+
+
 }
